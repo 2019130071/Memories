5a410c8165210edae75fef26a5c23ae9863fd00e
Akshay-Tarde <55123625+Akshay-Tarde@users.noreply.github.com>
2021-11-13 15:15:58 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use("/posts", postRoutes);
 
 const CONNECTION_URL =
-  "mongodb+srv://akshaytarde:akshay123123@cluster0.r2gkz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  "";
 const PORT = process.env.PORT || 5000;
 
 mongoose
